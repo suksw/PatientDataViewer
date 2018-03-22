d41de2c44a9dee69a6c978cc645aeb7df5d9968d
Subhashinie <subhashinieksw.14@cse.mrt.ac.lk>
2018-03-22 09:21:13 +0530

    urls changed to include observations

--- src/main/java/Config.java
+++ src/main/java/Config.java
@@ -1,13 +1,13 @@
 public class Config {
     //URLs
     public static String femalePatientsUrl
-            = "http://hapi.fhir.org/baseDstu3/Patient?_format=json&_count=3&gender=female&given=a";
+            = "http://hapi.fhir.org/baseDstu3/Patient?_format=json&_count=1&given=hy";
     public static String malePatientsUrl
-            = "http://hapi.fhir.org/baseDstu3/Patient?_format=json&_count=2&gender=male";
+            = "http://hapi.fhir.org/baseDstu3/Patient?_format=json&_count=4&gender=male";
     public static String encountersUrl
             = "http://hapi.fhir.org/baseDstu3/Encounter?_format=json&patient=";
     public static String observationsUrl
-            = "http://hapi.fhir.org/baseDstu3/Observation?_format=json&encounter=";
+            = "http://hapi.fhir.org/baseDstu3/Observation?_format=json&_count=10&encounter=";
 
     //File to store
     public static String patientsJsonFile = "src/main/resources/patients.json";
